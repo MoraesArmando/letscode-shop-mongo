51b67cdb74931227c8fca1b057c5c8fa1e9b816b
MoraesArmando <armando-am16@hotmail.com>
2022-05-06 17:23:04 -0300

    adicionando a seguranca

---
+++ shop/src/main/java/com/armando/shop/repository/UsersRepository.java
@@ -0,0 +1,10 @@
+package com.armando.shop.repository;
+
+import com.armando.shop.model.Users;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsersRepository extends MongoRepository<Users, String> {
+    Users findByUsername(String username);
+}
